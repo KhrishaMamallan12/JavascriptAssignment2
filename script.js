30bf202c22b7263d379d32390979d2b25680de7d
Khrisha Mamallan <125912478+KhrishaMamallan12@users.noreply.github.com>
2023-06-14 23:00:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+/* A. Create a variable “Skill Safari” and perform the following tasks.
+1) Find the length of the string.
+2) Convert the all the letters in the string into capital letters.
+3) What is the index position of “a”*/
+
+/*console.log("Captain America: Stark, we need a plan of attack!");
+console.log("Iron Man: I have a plan: attack!");
+
+console.log("Captain America: You're not the guy to make the sacrifice play, to lay down on a wire and let the other guy crawl over you.");
+console.log("Iron Man: I think I would just cut the wire.");
+
+console.log("Captain America: We need a diversion. Something big.");
+console.log("Iron Man: I got a nuke coming in. It's gonna blow in less than a minute. And I know just where to put it.");
+
+console.log("Captain America: Stark, you know that's a one-way trip.");
+console.log("Iron Man: Save the rest for the turn, JARVIS! Drop the needle!");
+
+console.log("Captain America: You're not the guy to make the sacrifice play, to lay down on a wire and let the other guy crawl over you.");
+console.log("Iron Man: I think I would just cut the wire.");
+
+console.log("Captain America: We need a diversion. Something big.");
+console.log("Iron Man: I got a nuke coming in. It's gonna blow in less than a minute. And I know just where to put it.");
+
+console.log("Captain America: Stark, you know that's a one-way trip.");
+console.log("Iron Man: Save the rest for the turn, JARVIS! Drop the needle!");*/
+
+
+/* B. Create a single consol.log that will display a famous conversation between Captain America 
+and Iron Man in Avengers. All the exchange should be in different lines.*/
+
+/*let phrase = "Skill Safari";
+
+// Task 1: Find the length of the string
+let length = phrase.length;
+console.log("Length of the string:", length);
+
+// Task 2: Convert all the letters in the string into capital letters
+let capitalizedPhrase = phrase.toUpperCase();
+console.log("Capitalized string:", capitalizedPhrase);
+
+// Task 3: Find the index position of "a"
+let index = phrase.indexOf("a");
+console.log("Index of 'a':", index);*/
+
+// C. Create a Body Mass Index logic by using Height and weight as inputs.
+/*let weight = 70; // weight in kilograms
+let height = 1.75; // height in meters
+
+// Calculate BMI
+let bmi = weight / (height * height);
+
+// Display BMI
+console.log("Weight:", weight, "kg");
+console.log("Height:", height, "m");
+console.log("BMI:", bmi.toFixed(2));*/
+
+
